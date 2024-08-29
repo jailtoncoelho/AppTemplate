ffae6d895a34a3929f58a4ff5c4898150346ea57
jailtoncoelho <jailtoncoelho@users.noreply.github.com>
2024-08-29 08:20:37 -0300

    Revert "renomeação da assinatura do aplicativo e refactoring"
    This reverts commit a252d4d3d8e16a2a3e6c880654d0c92503ab3eda.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.ifpr.app_reuse"
+    namespace = "com.jailton.apptemplateproject"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.ifpr.app_reuse"
+        applicationId = "com.jailton.apptemplateproject"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
--- app/src/main/java/com/ifpr/app_reuse/baseclasses/Usuario.kt
+++
@@ -1,8 +0,0 @@
-package com.ifpr.app_reuse.baseclasses
-
-data class Usuario(
-    var key: String? = null,
-    var nome: String? = null,
-    var email: String? = null,
-    var password: String? = null
-)
